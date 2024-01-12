98021e5ea13976cae82d7ed8d79124119bc0615e
Mateus Dias <testdredre@outlook.com>
2024-01-12 00:40:52 -0300

    App de Banco de Crédito

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,33 +1,78 @@
 ﻿using System;
 
-class Program
+internal class Program
 {
-    static void Main()
+    private static void Main(string[] args)
     {
+        int DisplayTelas_1 = 1;
+        string Entrar_cpf_cadastre = "123456789";
         int contador_repeticao = 0;
+        string senha_usuario_colocou = string.Empty;
 
-        while (contador_repeticao < 10)
+        while (contador_repeticao < 3)
         {
-            Console.WriteLine("Escreva seu nome:");
-            string Nome = Console.ReadLine();
+            Console.WriteLine("Digite SEU CPF: ");
+            string cpf = Console.ReadLine();
 
-            Console.WriteLine("Escreva sua idade:");
-            int Idade = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Escreva seu Peso:");
-            float Peso = Convert.ToSingle(Console.ReadLine());
-
-            if (Nome == "Mateus")
+            if (cpf == Entrar_cpf_cadastre)
             {
-                Console.WriteLine("Nome: " + Nome);
-                Console.WriteLine("Idade: " + Idade);
-                Console.WriteLine("Logado: " + Peso);
-                ++contador_repeticao;
-                continue;
+                Console.WriteLine("Digite sua senha: ");
+                senha_usuario_colocou = Console.ReadLine();
+
+                if (senha_usuario_colocou == "123456789")
+                {
+                    Console.WriteLine("Logado.... ");
+                    Console.WriteLine("Saldo: Eur:1000, Pix, Deposito, Trasferencia Bancaria, Doação, \n CDB (deposite e trasforme seu deposito em limite de credito");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha incorreta.");
+                }
             }
             else
             {
-                Console.WriteLine("Erro ao Logar, você vai receber um DDOS");
+                Console.WriteLine("CPF incorreto.");
+                contador_repeticao++;
+            }
+
+            if (contador_repeticao == 3)
+            {
+                Console.WriteLine("Limite de tentativas excedido. Conta bloqueada.");
+                break;
+            }
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Escolha uma opção de recuperação de senha:");
+            Console.WriteLine($"Digite {DisplayTelas_1} para recuperação de senha por email.");
+            Console.WriteLine("Digite 0 para sair.");
+
+            if (int.TryParse(Console.ReadLine(), out int selecioneaopcao))
+            {
+                switch (selecioneaopcao)
+                {
+                    case 0:
+                        Console.WriteLine("Saindo do sistema.");
+                        break;
+
+                    case 1:
+                        Console.WriteLine($"Senha enviada para o email do cliente: emailcliente@dominio.com");
+                        Console.WriteLine($"Verifique seu email, lixeira ou caixa de spawn");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        continue;
+                }
+
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Tente novamente.");
+                continue;
             }
         }
     }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ecf939891ae4535d80912305f4cd884dcdc7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
